2c7451808b44f2733481df9ba881d614538d0671
@RobLong03 <vasancol@gmail.com>
2025-02-13 17:14:07 +0100

    commit modifiche tutto l'homepage (da finire)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ProductComponent } from './Components/Products/product/product.component';
+import { HomeComponent } from './Components/HomePage/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'home', pathMatch:'full'},
+  {path:'home',component:HomeComponent},
+  {path:'product',component:ProductComponent},
+  //da cambiare
+  {path:'home',component:AppComponent},
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,29 +3,77 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import {MatTabsModule} from '@angular/material/tabs';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FlexLayoutServerModule } from '@angular/flex-layout/server';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+// Removed FlexLayoutServerModule import as it does not exist
+import { AdministratorComponent } from './Components/Administrator/administrator/administrator.component';
+import { LoginComponent } from './Components/Authtentication/login/login.component';
+import { RegisterComponent } from './Components/Authtentication/register/register.component';
+import { CartComponent } from './Components/Cart/cart/cart.component';
+import { ProductComponent } from './Components/Products/product/product.component';
+import { CaseComponent } from './Components/Products/case/case.component';
+import { HomeComponent } from './Components/HomePage/home/home.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import {MatMenuModule} from '@angular/material/menu';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdministratorComponent,
+    LoginComponent,
+    RegisterComponent,
+    CartComponent,
+    ProductComponent,
+    CaseComponent,
+    HomeComponent,
+    FooterComponent,
+
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterLink,
     RouterLinkActive,
-    RouterOutlet,
-    FormsModule,
-    ReactiveFormsModule,
-    MatTabsModule
-    
+    MatTabsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule,
+    FlexLayoutModule,
+    FlexLayoutServerModule,
+    MatDividerModule,
+    MatCardModule,
+    MatGridListModule,
+    MatMenuModule
   ],
   providers: [
     provideClientHydration(),
-    provideAnimationsAsync()
+    provideAnimationsAsync(),
+    provideHttpClient(withFetch())
   ],
   bootstrap: [AppComponent]
 })
