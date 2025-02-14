218fbaa43a7204d360149c2f7126d112e90afa97
@RobLong03 <vasancol@gmail.com>
2025-02-14 15:16:59 +0100

    changement route

--- src/app/Components/footer/footer.component.ts
+++ src/app/Components/footer/footer.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrl: './footer.component.css',
+  host: { 'hostID': crypto.randomUUID().toString() }
 })
 export class FooterComponent {
 
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './Components/Products/product/product.component';
 import { HomeComponent } from './Components/HomePage/home/home.component';
+import { CaseComponent } from './Components/Products/case/case.component';
 
 const routes: Routes = [
   {path:'',redirectTo:'home', pathMatch:'full'},
   {path:'home',component:HomeComponent},
   {path:'product',component:ProductComponent},
+  {path:'case',component:CaseComponent},
+
   //da cambiare
   {path:'home',component:AppComponent},
 
