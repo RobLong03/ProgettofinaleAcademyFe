f27a2031c9b39bbe2a7e005efd8793a5d55d2812
dl-01 <davidelodedo20@gmail.com>
2025-02-18 11:28:26 +0100

    Definizione componenti e definizione sistema di routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,18 +4,67 @@ import { AppComponent } from './app.component';
 import { ProductComponent } from './Components/Products/product/product.component';
 import { HomeComponent } from './Components/HomePage/home/home.component';
 import { CaseComponent } from './Components/Products/case/case.component';
+import { LoginComponent } from './Components/Authtentication/login/login.component';
+import { RegisterComponent } from './Components/Authtentication/register/register.component';
+import { AboutComponent } from './Components/about/about.component';
+import { CpuComponent } from './Components/Products/cpu/cpu.component';
+import { GpuComponent } from './Components/Products/gpu/gpu.component';
+import { MotherboardComponent } from './Components/Products/motherboard/motherboard.component';
+import { PsuComponent } from './Components/Products/psu/psu.component';
+import { RamComponent } from './Components/Products/ram/ram.component';
+import { StorageComponent } from './Components/Products/storage/storage.component';
+import { CustomerComponent } from './Components/Customer/customer/customer.component';
+import { CartComponent } from './Components/Customer/cart/cart.component';
+import { CheckoutComponent } from './Components/Customer/checkout/checkout.component';
+import { WishlistComponent } from './Components/Customer/wishlist/wishlist.component';
+import { AdminComponent } from './Components/Administration/admin/admin.component';
+import { ProductComponent as ProductAdministrationComponent} from './Components/Administration/product/product.component';
+import { CustomerComponent as CustomerAdministrationComponent} from './Components/Administration/customer/customer.component';
+import { OrdersComponent } from './Components/Administration/orders/orders.component';
+import { ErrorComponent } from './Components/error/error.component';
 
 const routes: Routes = [
-  {path:'',redirectTo:'home', pathMatch:'full'},
-  {path:'home',component:HomeComponent},
-  {path:'product',component:ProductComponent},
-  {path:'case',component:CaseComponent},
 
   //da cambiare
-  {path:'home',component:AppComponent},
+  {path:'home', component:AppComponent},
 
+  {path:'', redirectTo:'home', pathMatch:'full'},
+  {path:'home', component:HomeComponent},
+  {path:'login', component:LoginComponent},
+  {path:'register', component:RegisterComponent},
+  {path:'about', component:AboutComponent},
 
+  {path:'product', component:ProductComponent, children:[
+    {path:'case',component:CaseComponent},
+    {path:'case/:id',component:CaseComponent},
+    {path:'cpu', component:CpuComponent},
+    {path:'cpu/:id', component:CpuComponent},
+    {path:'gpu', component:GpuComponent},
+    {path:'gpu/:id', component:GpuComponent},
+    {path:'motherboard', component:MotherboardComponent},
+    {path:'motherboard/:id', component:MotherboardComponent},
+    {path:'psu', component:PsuComponent},
+    {path:'psu/:id', component:PsuComponent},
+    {path:'ram', component:RamComponent},
+    {path:'ram/:id', component:RamComponent},
+    {path:'storage', component:StorageComponent},
+    {path:'storage/:id', component:StorageComponent}
+  ]},
 
+  {path:'customer/:id', component:CustomerComponent, children:[
+    {path:'cart', component:CartComponent},
+    {path:'checkout', component:CheckoutComponent},
+    {path:'wishlist', component:WishlistComponent}
+  ]},
+  
+  {path:'admin', component:AdminComponent, children:[
+    {path:'product', component:ProductAdministrationComponent},
+    {path:'customer', component:CustomerAdministrationComponent},
+    {path:'orders', component:OrdersComponent}
+  ]},
+
+  {path:'404', component:ErrorComponent},
+  {path:'**', redirectTo:'404'}
 ];
 
 @NgModule({
