312c076bec163adbb0e3bde0f3b47b1a05bbbe16
dl-01 <davidelodedo20@gmail.com>
2025-02-27 10:47:47 +0100

    Agiunta logica per gestione bottone login/logout sulla navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { isPlatformBrowser } from '@angular/common';
 import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { AuthServiceService } from './Auth/auth-service.service';
 
 @Component({
   selector: 'app-root',
@@ -11,14 +12,20 @@ export class AppComponent implements OnInit {
   title = 'ProjectBetaFE';
   admin: boolean = false;
   islogged: boolean = false;
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private authS:AuthServiceService
+  ) { }
 
   ngOnInit(): void {
     if (isPlatformBrowser(this.platformId)) {
-      this.admin = Boolean(localStorage.getItem('isAdmin'));
-      this.islogged = Boolean(localStorage.getItem('isLoggedIn'));
+      this.admin=this.authS.isAuthenticatedAdmin();
+      this.islogged=this.authS.isAuthenticatedCustomer();
     }
   }
 
+  logout() {
 
+    this.authS.resetAll();
+  };
 }
