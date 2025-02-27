a81beef929248024d76f3db2bf97b6914ec368ca
SerDam19 <serinidamiano@gmail.com>
2025-02-27 16:30:20 +0100

    migliorie carrello

--- src/app/Components/Customer/cart/cart.component.ts
+++ src/app/Components/Customer/cart/cart.component.ts
@@ -3,6 +3,7 @@ import { CartItemService } from '../../../services/cart/cart-item.service';
 import { CartService } from '../../../services/cart/cart.service';
 import { ActivatedRoute } from '@angular/router';
 import { SessionStorageService } from '../../../utils/session-storage.service';
+import { ProductService } from '../../../services/products/product.service';
 
 @Component({
   selector: 'app-cart',
@@ -13,28 +14,47 @@ export class CartComponent implements OnInit {
   items: any;
   customerId: number | null;
   cartId: number | null;
+  totalPrice: number | null | undefined;
 
   constructor(
     private cartItem: CartItemService,
     private cart: CartService,
     private route: ActivatedRoute,
-    private userValues:SessionStorageService
+    private userValues: SessionStorageService,
+    private productS: ProductService
   ) {
     this.customerId = parseInt(this.userValues.idCliente!);
     this.cartId = parseInt(this.userValues.idCarrelloCliente!);
   }
 
   ngOnInit(): void {
-    
-    this.cart.getCart(this.customerId!)
-      .subscribe((resp:any) => {
+    this.cart.getCart(this.customerId!).subscribe((resp: any) => {
+      this.items = resp.dati.items;
+      this.totalPrice = resp.dati.totalPrice;
+      this.items.forEach((item: any) => {
+        this.productS.getProduct(item.productId).subscribe((product: any) => {
+          item.product = product;
+        });
+      });
+      console.log(this.items);
+    });
+  }
 
-      this.items=resp.dati.cartItemS;
+  updateDati() {
+    this.cart.getCart(this.customerId!).subscribe((resp: any) => {
+      this.items = resp.dati.items;
+      this.totalPrice = resp.dati.totalPrice;
+      this.items.forEach((item: any) => {
+        this.productS.getProduct(item.productId).subscribe((product: any) => {
+          item.product = product;
+        });
+      });
       console.log(this.items);
     });
   }
 
   deleteItem(id: number) {
+    console.log(id);
     this.cartItem
       .removeCartItem({
         id: id,
@@ -42,22 +62,57 @@ export class CartComponent implements OnInit {
       .subscribe((resp: any) => {
         console.log(resp.rc);
         if (resp.rc) {
-          this.items = this.items.filter((it: any) => it.id !== id);
+          this.updateDati();
         }
       });
   }
 
   emptyCart() {
-    this.cart
-      .clearCart({
-        id: parseInt(this.route.parent?.snapshot.paramMap.get('id')!),
-      })
-      .subscribe((resp: any) => {
-        console.log(resp.rc);
-        if (resp.rc) {
-          this.items = [];
-        }
-      });
+    const cartId = this.cartId;  // Usa il cartId che hai già salvato nel componente
+    if (cartId) {
+      // Chiamata al servizio per svuotare il carrello
+      this.cart.clearCart({ id: cartId })
+        .subscribe((resp: any) => {
+          console.log("Risposta clearCart:", resp);
+          if (resp.rc) {
+            this.updateDati();  // Svuota gli articoli dal carrello
+          }
+        });
+    } else {
+      console.error("Carrello ID non valido!");
+    }
+  }
+
+  addQuantity(id: number) {
+    // Trova l'articolo nel carrello
+    const item = this.items.find((it: any) => it.id === id);
+
+    if (item) {
+      // Aumenta la quantità
+      const updatedQuantity = item.quantity + 1;
+
+      // Chiamata al servizio per aggiornare la quantità nel carrello
+      this.cartItem
+        .addCartItem({
+          id: id,
+          quantity: updatedQuantity,
+        })
+        .subscribe(
+          (resp: any) => {
+            // Se la risposta è positiva, aggiorna la quantità
+            if (resp.rc) {
+              this.updateDati();
+            } else {
+              // Puoi gestire eventuali errori in caso di risposta negativa
+              console.error("Errore nell'aggiornamento della quantità", resp);
+            }
+          },
+          (error: any) => {
+            // Gestione degli errori nel caso di errore nella richiesta
+            console.error('Errore di rete o altro:', error);
+          }
+        );
+    }
   }
 
   reduceItem(id: number) {
@@ -74,14 +129,14 @@ export class CartComponent implements OnInit {
           })
           .subscribe((resp: any) => {
             if (resp.rc) {
-              item.quantity--;
+              this.updateDati();
             }
           });
       } else {
         // Se la quantità è 1, lo rimuove completamente
         this.cartItem.removeCartItem({ id: id }).subscribe((resp: any) => {
           if (resp.rc) {
-            this.items = this.items.filter((it: any) => it.id !== id);
+            this.updateDati();
           }
         });
       }
